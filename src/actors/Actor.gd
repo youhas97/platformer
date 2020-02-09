095d68d263b6f1840fbcc4387164a9caea8b03a8
Yousef Hashem <youha847@student.liu.se>
2020-02-09 23:17:28 +0100

    Initial commit

---
+++ src/actors/Actor.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(500.0, 800.0)
+export var gravity: = 1500.0
+var _velocity: = Vector2.ZERO
